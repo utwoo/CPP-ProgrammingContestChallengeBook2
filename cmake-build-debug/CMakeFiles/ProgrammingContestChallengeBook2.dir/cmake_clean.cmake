6e8d3a79e5325dbd33facfa13831a370ee7b0f3e
Xiang Zhu <zhuxiang1212@hotmail.com>
2021-10-16 13:54:12 +0800

    Initialization

---
+++ cmake-build-debug/CMakeFiles/ProgrammingContestChallengeBook2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgrammingContestChallengeBook2.dir/1-1/draw-lots.cpp.obj"
+  "ProgrammingContestChallengeBook2.exe"
+  "ProgrammingContestChallengeBook2.exe.manifest"
+  "ProgrammingContestChallengeBook2.pdb"
+  "libProgrammingContestChallengeBook2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgrammingContestChallengeBook2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
